258a91e8db219435250f288c581bfbc5209e0ef0
sirriah <tereza.dvorakova.3@seznam.cz>
2021-09-10 23:55:06 +0200

    location api and details styling

--- js/location.js
+++ js/location.js
@@ -1,14 +1,17 @@
+/* eslint-disable camelcase */
 /*
 Location API
 */
 
+const locationCity = document.getElementById('locationCity');
 
-fetch('https://reallyfreegeoip.org/json/')
-    .then((data) => data.json())
-    .then((d) = writeOutData(d))
-    .catch((error) => console.log(error.message));
-
-
-function writeOutData(locationData){
-    console.log(locationData);
+function writeOutData(locationData) {
+  const { country_code, city } = locationData;
+  locationCity.innerHTML = `${city}, ${country_code}`;
 }
+
+fetch('https://freegeoip.app/json/')
+  .then((data) => data.json())
+  .then((d) => writeOutData(d))
+  // eslint-disable-next-line no-console
+  .catch((error) => console.error(error.message));
--- js/time.js
+++ js/time.js
@@ -1,43 +1,37 @@
+/* eslint-disable no-console */
+/* eslint-disable camelcase */
 /*
-World Time API 
+World Time API
 */
 
 // the elements from index.html / day details
-const detailDayOfWeek = document.getElementById("details__day-of-week");
-const detailDayOfYear = document.getElementById("details__day-of-year");
-const detailWeekNumber = document.getElementById("details__week-number");
-const detailTimezone = document.getElementById("details__timezone");
+const detailDayOfWeek = document.getElementById('details__day-of-week');
+const detailDayOfYear = document.getElementById('details__day-of-year');
+const detailWeekNumber = document.getElementById('details__week-number');
+const detailTimezone = document.getElementById('details__timezone');
 // the time parts
-const timeDigital = document.getElementById("time");
-const timeAbbr = document.getElementById("timezone-abbr");
+const timeDigital = document.getElementById('time');
+const timeAbbr = document.getElementById('timezone-abbr');
 
-
-//fetching data
-fetch("https://worldtimeapi.org/api/ip")
-  .then((data) => data.json())
-  .then((time) => parseDataTime(time))
-  .catch((err) => console.log(err.message));
-
-
+// fetching data
 
 function parseDataTime(time) {
 // destructuring the fetched object
   const {
     datetime,
     abbreviation,
-    client_ip,
     day_of_week,
     day_of_year,
     week_number,
     timezone,
   } = time;
 
-//setClientIP(client_ip);
+  // setClientIP(client_ip);
 
-// re-format the date object
-  const timeNumbers = new Intl.DateTimeFormat("cs-CZ", {
-    hour: "numeric",
-    minute: "numeric",
+  // re-format the date object
+  const timeNumbers = new Intl.DateTimeFormat('cs-CZ', {
+    hour: 'numeric',
+    minute: 'numeric',
   }).format(new Date(datetime));
 
   // the time object literal
@@ -51,4 +45,7 @@ function parseDataTime(time) {
   detailTimezone.innerHTML = timezone;
 }
 
-
+fetch('https://worldtimeapi.org/api/ip')
+  .then((data) => data.json())
+  .then((time) => parseDataTime(time))
+  .catch((err) => console.error(err.message));
