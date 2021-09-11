08c3536e2a8fb1a25a4b3783bf5400e3f81efe7a
sirriah <tereza.dvorakova.3@seznam.cz>
2021-09-11 14:06:31 +0200

    restoration

--- js/quote.js
+++ js/quote.js
@@ -3,26 +3,25 @@ const authorQuote = document.getElementById('quote__author');
 const contentQuote = document.getElementById('quote__content');
 const buttonQuote = document.getElementById('quote__button');
 
-const quoteDefault = {author : 'Ada Lovelace', content : 'The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.'}
+const quoteDefault = { author: 'Ada Lovelace', content: 'The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.' };
+const processQuote = (quote) => {
+  authorQuote.innerHTML = quote.author;
+  contentQuote.innerHTML = quote.content;
+};
+
+// calling the fetching again - refreshing button
 
 // fetching the data
 const fetchingApi = () => {
-    fetch('https://api.quotable.io/random')
-    .then(data => data.json())
-    .then(quote => processQuote(quote))
-    .catch(err => processQuote(quoteDefault));
+  fetch('https://api.quotable.io/random')
+    .then((data) => data.json())
+    .then((quote) => processQuote(quote))
+    .catch(() => processQuote(quoteDefault));
+};
 
-}
-
+buttonQuote.addEventListener('click', () => {
+  fetchingApi();
+});
 // filling the elements with data
-const processQuote = (quote) => {
-    authorQuote.innerHTML = quote.author;
-    contentQuote.innerHTML = quote.content;
-}
-
-//calling the fetching again - refreshing button
-buttonQuote.addEventListener('click', e => {
-    fetchingApi();
-})
 
 fetchingApi();
