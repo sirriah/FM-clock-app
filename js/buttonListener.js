2a378a4afa2c5c5c8e08a86629a9b18ad2a2a099
sirriah <tereza.dvorakova.3@seznam.cz>
2021-09-16 13:57:20 +0200

    hover effect button

--- js/buttonListener.js
+++ js/buttonListener.js
@@ -10,12 +10,12 @@ button.addEventListener('click', (e) => {
   e.preventDefault();
   if (isUp) {
     window.scrollBy(0, window.innerHeight);
-    button.innerHTML = 'Less <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle fill="#303030" cx="20" cy="20" r="20"/><path stroke="#FFF" stroke-width="2" d="M14 23l6-6 6 6"/></g></svg>';
+    button.innerHTML = 'Less <img src="./assets/desktop/icon-arrow-up.svg" alt="" />';
     button.classList.remove('rotation');
     isUp = false;
   } else {
     window.scrollBy(0, -window.innerHeight);
-    button.innerHTML = 'More <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle fill="#303030" cx="20" cy="20" r="20"/><path stroke="#FFF" stroke-width="2" d="M14 23l6-6 6 6"/></g></svg>';
+    button.innerHTML = 'More <img src="./assets/desktop/icon-arrow-up.svg" alt="" />';
     button.classList.add('rotation');
     isUp = true;
   }
@@ -30,13 +30,13 @@ https://stackoverflow.com/questions/1145850/how-to-get-height-of-entire-document
 */
 window.addEventListener('scroll', () => {
   if (window.scrollY === 0) {
-    button.innerHTML = 'More <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle fill="#303030" cx="20" cy="20" r="20"/><path stroke="#FFF" stroke-width="2" d="M14 23l6-6 6 6"/></g></svg>';
+    button.innerHTML = 'More <img src="./assets/desktop/icon-arrow-up.svg" alt="" />';
     button.classList.add('rotation');
     isUp = true;
   }
 
   if (document.documentElement.scrollHeight === window.pageYOffset + window.innerHeight) {
-    button.innerHTML = 'Less <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle fill="#303030" cx="20" cy="20" r="20"/><path stroke="#FFF" stroke-width="2" d="M14 23l6-6 6 6"/></g></svg>';
+    button.innerHTML = 'Less <img src="./assets/desktop/icon-arrow-up.svg" alt="" />';
     button.classList.remove('rotation');
     isUp = false;
   }
