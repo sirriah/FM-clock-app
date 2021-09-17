3dec64e207942c9926238dee340e2bee96abafe3
sirriah <tereza.dvorakova.3@seznam.cz>
2021-09-17 14:13:23 +0200

    background attachment

--- js/quote.js
+++ js/quote.js
@@ -3,14 +3,14 @@ Api Quotable - returns the quotes
 */
 
 // the elements from the index.html
-const authorQuote = document.getElementById('quote__author');
+// const authorQuote = document.getElementById('quote__author');
 const contentQuote = document.getElementById('quote__content');
 const buttonQuote = document.getElementById('quote__button');
 
 const quoteDefault = { author: 'Ada Lovelace', content: 'The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.' };
 const processQuote = (quote) => {
-  authorQuote.innerHTML = quote.author;
-  contentQuote.innerHTML = quote.content;
+  // authorQuote.innerHTML = quote.author;
+  contentQuote.innerHTML = `${quote.content} <h5 id="quote__author">${quote.author}</h5>`;
 };
 
 // calling the fetching again - refreshing button
